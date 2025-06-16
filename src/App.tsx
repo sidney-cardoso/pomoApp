26656ae6e2ff15044dc812f764ae8acbb39557ab
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-16 20:59:44 -0300

    Componentes em pastas padronizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { Heading } from './Heading';
 
 export function App() {
 	return (
