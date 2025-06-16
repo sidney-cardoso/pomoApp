6d0b1a6de711764956803f2c39639956bec18913
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-16 20:41:02 -0300

    Estrutura de cada seção

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
 	return (
 		<>
-			<Heading>
-				Welcome to PomoAPP!
-				<button>
-					<TimerIcon />
-				</button>
-			</Heading>
-			<p>
-				This is a simple Pomodoro timer application built with React. It
-				helps you manage your time effectively by breaking work into
-				intervals, traditionally 25 minutes in length, separated by
-				short breaks.
-			</p>
+			<div className='container'>
+				<div className='content'>
+					<section>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Commodi numquam quas mollitia ipsum libero eos
+							quo a fuga, accusamus deserunt voluptates error nemo
+							ut vero excepturi maxime non nulla aut?
+						</p>
+					</section>
+				</div>
+			</div>
 		</>
 	);
 }
