99592cd38230428e18597e5619c389f186a57dd3
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-18 21:16:20 -0300

    Componente de countdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
 	return (
@@ -13,6 +14,9 @@ export function App() {
 			</Container>
 			<Container>
 				<Menu />
+			</Container>
+			<Container>
+				<CountDown />
 			</Container>
 		</>
 	);
