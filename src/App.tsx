fc67743e69c4ab1ce1692ede6427e8b0cf434e6b
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-18 21:24:41 -0300

    Estrutura do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,35 @@ export function App() {
 			<Container>
 				<Logo />
 			</Container>
+
 			<Container>
 				<Menu />
 			</Container>
+
 			<Container>
 				<CountDown />
+			</Container>
+
+			<Container>
+				<form action='' className='form'>
+					<div className='form-row'>
+						<label htmlFor='input'>Task</label>
+						<input type='text' id='input' />
+					</div>
+
+					<div className='form-row'>
+						<p>Lorem ipsum dolor sit amet.</p>
+					</div>
+
+					<div className='form-row'>
+						<p>Ciclos</p>
+						<p>0 0 0 0 0 0</p>
+					</div>
+
+					<div className='form-row'>
+						<button>Enviar</button>
+					</div>
+				</form>
 			</Container>
 		</>
 	);
