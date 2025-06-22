2e2d171f5bf75edc03fc03970aee0777910f7c7a
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-22 11:02:01 -0300

    Armazenando o tema no Local Storage

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,11 +1,22 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+	HistoryIcon,
+	HouseIcon,
+	MoonIcon,
+	SettingsIcon,
+	SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
 import { useEffect, useState } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-	const [theme, setTheme] = useState<AvailableThemes>('dark');
+	const [theme, setTheme] = useState<AvailableThemes>(() => {
+		const storageTheme =
+			(localStorage.getItem('theme') as AvailableThemes) || 'dark';
+
+		return storageTheme;
+	});
 
 	const handleThemeChange = (
 		event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -20,6 +31,7 @@ export function Menu() {
 
 	useEffect(() => {
 		document.documentElement.setAttribute('data-theme', theme);
+		localStorage.setItem('theme', theme);
 	}, [theme]);
 
 	return (
@@ -55,7 +67,7 @@ export function Menu() {
 				title='Mudar Tema'
 				onClick={handleThemeChange}
 			>
-				<SunIcon />
+				{theme == 'dark' ? <SunIcon /> : <MoonIcon />}
 			</a>
 		</nav>
 	);
