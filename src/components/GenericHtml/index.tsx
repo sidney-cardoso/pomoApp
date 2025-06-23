20d21f5548272d4c6a573f0f87ff51218aeb41b5
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-22 22:43:29 -0300

    HTML genérico

---
+++ src/components/GenericHtml/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+type GeneticHtmlProps = {
+	children: React.ReactNode;
+};
+
+export function GenericHtml({ children }: GeneticHtmlProps) {
+	return <div className={styles['generic-html']}>{children}</div>;
+}
