28dedc7d93aaab4c50f773e8f3f0922fbbd769b0
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-06-30 20:06:58 -0300

    Formatando cronômetro

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
+import { formatSecondsToMinutes } from '../../utils/formatSecondsToMinutes';
 
 export function Form() {
 	const { state, setState } = useTaskContext();
@@ -47,7 +48,8 @@ export function Form() {
 				activeTask: newTask,
 				currentCycle: nextCycle,
 				secondsRemaining,
-				formattedSecondsRemaining: '00:00',
+				formattedSecondsRemaining:
+					formatSecondsToMinutes(secondsRemaining),
 				tasks: [...prevState.tasks, newTask],
 			};
 		});
