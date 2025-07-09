39ce502c43fefbb2ce1a6d251569344072e54dc0
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-07-08 23:20:55 -0300

    Tocando áudio ao finalizar tarefa

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialTaskState';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/timeWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
 	children: React.ReactNode;
@@ -11,6 +12,7 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+	const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
 	const worker = TimerWorkerManager.getInstance();
 
@@ -20,6 +22,11 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 		console.log(countdownSeconds);
 
 		if (countdownSeconds <= 0) {
+			if (playBeepRef.current) {
+				playBeepRef.current();
+				playBeepRef.current = null;
+			}
+
 			dispatch({
 				type: TaskActionTypes.COMPLETE_TASK,
 			});
@@ -34,15 +41,20 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	});
 
 	useEffect(() => {
-		console.log(state);
-
 		if (!state.activeTask) {
-			console.log('Worker terminado por falta de task ativa');
 			worker.terminate();
 		}
 
 		worker.postMessage(state);
 	}, [worker, state]);
+
+	useEffect(() => {
+		if (state.activeTask && playBeepRef.current === null) {
+			playBeepRef.current = loadBeep();
+		} else {
+			playBeepRef.current = null;
+		}
+	}, [state.activeTask]);
 
 	return (
 		<TaskContext.Provider value={{ state, dispatch }}>
