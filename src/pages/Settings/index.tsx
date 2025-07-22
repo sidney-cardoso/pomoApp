d91dd53318a1b6cb7e7e759ae13306f37a110075
Sidney Cardoso <sidneyrsc342@gmail.com>
2025-07-21 22:43:19 -0300

    Validação do formulário

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,12 +4,14 @@ import { Heading } from '../../components/Heading';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+
 import { SaveIcon } from 'lucide-react';
 import { useRef } from 'react';
 
 import '../../styles/global.css';
 import '../../styles/theme.css';
-import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { showMessage } from '../../adapters/showMessage';
 
 export function Settings() {
 	const { state } = useTaskContext();
@@ -20,10 +22,38 @@ export function Settings() {
 
 	function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
+		showMessage.dismiss();
 
-		const workTime = workTimeInput.current?.value;
-		const shortBreakTime = shortBreakTimeInput.current?.value;
-		const longBreakTime = longBreakTimeInput.current?.value;
+		const workTime = Number(workTimeInput.current?.value);
+		const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+		const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+		const formErrors: string[] = [];
+
+		if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+			showMessage.warn('Digite apenas números nos campos!');
+		}
+
+		if (workTime < 10 || workTime > 90) {
+			formErrors.push('O tempo de foco deve ser entre 10 e 90 minutos!');
+		}
+		if (shortBreakTime < 5 || shortBreakTime > 30) {
+			formErrors.push(
+				'O tempo de descanso curto deve ser entre 5 e 30 minutos!',
+			);
+		}
+		if (longBreakTime < 10 || longBreakTime > 60) {
+			formErrors.push(
+				'O tempo de descanso longo deve ser entre 10 e 60 minutos!',
+			);
+		}
+		if (formErrors.length > 0) {
+			formErrors.forEach(error => {
+				showMessage.error(error);
+			});
+			return;
+		}
+		showMessage.info('Configurações salvas com sucesso!');
 	}
 
 	return (
@@ -47,6 +77,7 @@ export function Settings() {
 							labelText='Foco'
 							ref={workTimeInput}
 							defaultValue={state.config.workTime}
+							type='number'
 						/>
 					</div>
 
@@ -56,6 +87,7 @@ export function Settings() {
 							labelText='Descanso curto'
 							ref={shortBreakTimeInput}
 							defaultValue={state.config.shortBreakTime}
+							type='number'
 						/>
 					</div>
 
@@ -65,6 +97,7 @@ export function Settings() {
 							labelText='Descanso longo'
 							ref={longBreakTimeInput}
 							defaultValue={state.config.longBreakTime}
+							type='number'
 						/>
 					</div>
 
